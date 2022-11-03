36faa1be9922dc55f487262fb0026cde6a656d50
Adedotun <seguntimileyin489@gmail.com>
2022-11-03 19:39:11 +0100

    Adding files for project one, project 2 continuation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ProfileHeader from "./profilesection/profilesection";
+import LinkSection from "./linksection/linksection";
+import BeforeFooter from "./sectionbforefooter/sectionbeforefooter";
+import Footer from "./footersection/footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ProfileHeader />
+      <LinkSection />
+      <BeforeFooter />
+      <Footer />
     </div>
   );
 }
